af70da7c0aff81628c17af225a535a82d3584c16
shtafinsky <s.shtafinsky@mail.ch>
2026-05-30 11:47:03 +0200

    Add files via upload

--- app/process_invoices_demo.py
+++ app/process_invoices_demo.py
@@ -794,27 +794,29 @@ def calculate_development(df,df_prev,cur_year,prev_year):
 # ------------------------ Main routine ----------------------------------
 # Existierende Jahren aus den Rechnungsdateien ermitteln
 # ----------------------------------------------------------------------- 
-
-config = load_config()
-
-if not config:
     if app_config.DEMO_MODE:
         folder = Path(__file__).resolve().parents[1] / "demo_data"
     else:
-        st.title("Setup")
+        config = load_config()
 
-        folder = st.text_input("Bitte geben Sie den Datenordner an")
+        if not config:
+            st.title("Setup")
 
-        submitted=st.button("Speichern")
+            folder = st.text_input("Bitte geben Sie den Datenordner an")
 
-    if not folder:
-        st.stop()
+            submitted=st.button("Speichern")
 
-    save_config(folder)
-    st.rerun()
+            if not folder:
+                st.stop()
 
-if config not in st.session_state:
-    st.session_state["config"] = config  
+            save_config(folder)
+            st.rerun()
+
+        if config not in st.session_state:
+            st.session_state["config"] = config  
+
+    st.write("DEMO_MODE:", app_config.DEMO_MODE)
+    st.write("folder type:", type(folder), folder)
 
 # initialization
 init_state()
